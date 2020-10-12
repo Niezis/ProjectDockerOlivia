026ef6c4078d0a6cd58d8cf8d6aa8db6ebb38e5f
Muhammad Agung Zuhdi <janissaryagung@gmail.com>
2020-10-12 17:11:10 +0700

    Post Show

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,22 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-
-            <div class="card mb-3 " style="width: 320px;">
+    <div class="row justify-content-center" >
+        <div class="col-md-6 ">
+            <div class="card mb-3 " style="width: 683px;">
                 <div class="card-header">
                     {{ $category->category }}
 
                 </div>
-
-
                 {{-- <video width="320" height="240" class='p-2' controls>
                     <source src="{{ asset("storage/".$post->video) }}" type="video/mp4">
                 </video> --}}
 
                 <iframe id="existing-iframe-example"
-                width="320" height="240"
+                width="683" height="384"
 
                 @if ($post->start)
                     src="https://www.youtube.com/embed/{{ $link }}?start={{$post->start}}&end={{$post->end}}"
@@ -33,7 +30,10 @@
                     <div class="p-3 mb-2 bg-secondary text-white">{{$post->description}}
                     </div>
                 </div>
-
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-3 " style="width: 200px;">
                 <div class="card-footer">
                     @foreach ($category->posts as $post)
                     <a href="/posts/{{$post->slug}}/show" class="list-group-item list-group-item-action list-group-item-secondary">{{ $post->name_video }}</a>
